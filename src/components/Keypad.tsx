ce3081da22718639550fae3a90c459a04840415a
Isaac Almanza <isaac.almanza19@gmail.com>
2024-10-16 10:35:18 -0500

    feat: update keypad design

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -7,7 +7,7 @@ interface KeypadProps {
 }
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
-const BUTTON_SIZE = screenWidth * 0.2;
+const BUTTON_SIZE = screenWidth * 0.17;
 const VERTICAL_GAP = screenHeight * 0.01;
 
 const Keypad: React.FC<KeypadProps> = ({ onKeyPress }) => {
@@ -24,7 +24,6 @@ const Keypad: React.FC<KeypadProps> = ({ onKeyPress }) => {
                 <View key={rowIndex} style={styles.row}>
                     {row.map((key, columnIndex) => (
                         <TouchableOpacity key={columnIndex} style={styles.keypadButton} onPress={() => onKeyPress(key)}>
-
                             {key === 'backspace' ? (
                                 <Ionicons name="backspace-outline" size={24} color="white" />
                             ) : (
@@ -43,7 +42,7 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'space-between',
         paddingBottom: VERTICAL_GAP,
-        marginBottom: 10
+        marginBottom: 15
     },
     row: {
         flexDirection: 'row',
